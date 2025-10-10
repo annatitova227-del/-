ff8c6156733f8489a19d9e6fd81210c49e0265e0
Ana Titova <annatitova227@gmail.com>
2025-10-10 18:07:32 +0300

    feat: update app

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,13 @@ import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import UserProfileScreen from './src/screens/UserProfileScreen';
-import TabNavigator from './src/TabNavigator';
+import UserProfileScreen from './UserProfileScreen';
+import TabNavigator from './TabNavigator';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  
   return (
     <SafeAreaProvider>
       <NavigationContainer>
